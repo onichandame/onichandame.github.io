c03633632d276ad5f29a6bc3e783c046e7e68662
onichandame <zxinmyth@gmail.com>
2020-06-01 14:47:35 +0000

    started dynamic particles

--- src/components/Layout/Background/Particle.tsx
+++ src/components/Layout/Background/Particle.tsx
@@ -1,13 +1,20 @@
 // particle rotating around a center point
 import React, { FC, useRef, ComponentProps } from "react"
+import { useFrame } from "react-three-fiber"
 import { Mesh, Color } from "three"
 
 type Props = ComponentProps<"mesh">
 
 export const Particle: FC<Props> = props => {
-  const box = useRef<Mesh>(null)
+  const particle = useRef<Mesh>(null)
+  useFrame(() => {
+    if (particle && particle.current) {
+      const model = particle.current
+      model.position.y += 0.1
+    }
+  })
   return (
-    <mesh ref={box} {...props}>
+    <mesh ref={particle} {...props}>
       <sphereBufferGeometry attach="geometry" args={[0.01, 32, 32]} />
       <meshPhysicalMaterial
         emissive={new Color(0x00ff00)}
