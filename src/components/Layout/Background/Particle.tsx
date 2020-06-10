34ccd83b7f0988bf06534e8612950b3ee6da805b
onichandame <zxinmyth@gmail.com>
2020-06-10 16:35:28 +0000

    done background. doing layout design

--- src/components/Layout/Background/Particle.tsx
+++ src/components/Layout/Background/Particle.tsx
@@ -1,26 +1,17 @@
 // particle rotating around a center point
 import React, { FC, useRef, ComponentProps } from "react"
-import { useFrame } from "react-three-fiber"
 import { Mesh, Color } from "three"
 
 type Props = ComponentProps<"mesh">
 
 export const Particle: FC<Props> = props => {
   const particle = useRef<Mesh>(null)
-  const speed = 0.1 + 0.1 * Math.random()
-  const animate = () => {
-    const model = particle.current
-    model.position.y += speed
-  }
-  useFrame(() => {
-    animate()
-  })
   return (
     <mesh ref={particle} {...props}>
-      <sphereBufferGeometry attach="geometry" args={[0.01, 32, 32]} />
+      <sphereBufferGeometry attach="geometry" args={[0.05, 32, 32]} />
       <meshPhysicalMaterial
         emissive={new Color(0x00ff00)}
-        emissiveIntensity={1}
+        emissiveIntensity={5}
         reflectivity={1}
         color={"blue"}
         attach="material"
--- src/components/Layout/Background/Particles.tsx
+++ src/components/Layout/Background/Particles.tsx
@@ -9,41 +9,52 @@ type Props = {
   count: number
 }
 
-const center = new Vector3(0, 0, -5)
-const radius = 10
+const width = 25
+const depth = 5
+const height = 15
+const center = new Vector3(0, 0, 0)
+
+const genStartPoint = () => {
+  return new Vector3(
+    Math.random() * width - width / 2 + center.x,
+    Math.random() * height - height / 2 + center.y,
+    Math.random() * depth - depth / 2 + center.z
+  )
+}
 
 export const Particles: FC<Props> = ({ count }) => {
   const group = useRef<Group>(null)
   const particles = useMemo<{ component: React.ReactNode }[]>(() => {
     let result = []
     for (let i = 0; i < count; ++i) {
-      const angle = Math.random() * 2 * Math.PI
-      const distance = Math.random() * radius
       result.push({
-        component: (
-          <Particle
-            position={[
-              Math.cos(angle) * distance,
-              Math.sin(angle) * distance,
-              Math.sin(angle) * distance
-            ]}
-          />
-        )
+        component: <Particle position={genStartPoint()} />
       })
     }
     return result
   }, [count])
+  const speeds: number[] = []
+  for (let i = 0; i < count; ++i) speeds.push(Math.random() * 0.01 + 0.01)
   const animate = () => {
-    group.current.children.forEach(particle => {
-      particle.position.y += Math.random() * 0.1 + 0.1
-      if (particle.position.y > 5) particle.position.y = 0
+    group.current.children.forEach((particle, i) => {
+      particle.position.y += speeds[i]
+      //particle.position.x += Math.random() * 0.2 - 0.1
+      if (
+        particle.position.y - center.y > height / 2 ||
+        Math.abs(particle.position.x) - center.x > width / 2
+      ) {
+        const birthPoint = genStartPoint()
+        particle.position.x = birthPoint.x
+        particle.position.y = birthPoint.y
+        particle.position.z = birthPoint.z
+      }
     })
   }
   useFrame(() => {
     animate()
   })
   return (
-    <group ref={group} position={center.toArray() as [number, number, number]}>
+    <group ref={group} position={center}>
       {particles.map(({ component }) => component)}
     </group>
   )
