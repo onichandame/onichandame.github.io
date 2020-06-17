363d900dda5b5a59e6559403fdcc9569ce7fd39a
onichandame <zxinmyth@gmail.com>
2020-06-17 13:10:57 +0000

    about

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react"
+import { Grid } from "@material-ui/core"
 
 const ContactPage: FC = () => {
-  return <div>hi</div>
+  return (
+    <Grid container direction={"column"}>
+      <Grid item>
+        email: <a href={"mailto:zxinmyth@gmail.com"}>zxinmyth@gmail.com</a>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default ContactPage
